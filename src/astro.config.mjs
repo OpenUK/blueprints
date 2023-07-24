fb3a0567365a769f1d39e223c7de93e33dd996de
Chris Lloyd-Jones <19361656+Sealjay@users.noreply.github.com>
2023-07-24 17:28:51 +0100

    Adds deploy for github pages

--- src/astro.config.mjs
+++ src/astro.config.mjs
@@ -4,5 +4,7 @@ import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://openuk.github.io',
+  base: '/blueprints',
   integrations: [tailwind()]
 });
